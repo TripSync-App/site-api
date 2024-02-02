02ff33b8292dd4c50d0a5e9425a0f53eae3f5311
Jayden Pyles <jpylesbusiness@gmail.com>
2024-02-01 18:39:07 -0600

    wip: add individual message query

--- src/site_api/routes/MessageRoutes.py
+++ src/site_api/routes/MessageRoutes.py
@@ -1,3 +1,5 @@
+import json
+
 from fastapi import APIRouter, Request
 
 from site_api.edgedb import DatabaseFunctions as dbf
@@ -8,6 +10,20 @@ message_router = APIRouter()
 @message_router.get("/messages")
 async def get_messages():
     return await dbf.query("SELECT default::Message {**};")
+
+
+@message_router.get("/messages/{message_id}")
+async def get_individual_message(message_id: int):
+    message = await dbf.query(
+        f"SELECT default::Message{{**}} FILTER .message_id = {message_id};",
+        query_single=True,
+    )
+
+    if isinstance(message, str):
+        message = json.loads(message)
+        message["author"].pop("password")
+
+    return message
 
 
 @message_router.post("/messages")
